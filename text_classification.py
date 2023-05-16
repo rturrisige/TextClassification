0cecd41575f4653202454ce94481fbf9512ec94b
rturrisi <rosanna.turrisi@edu.unige.it>
2023-05-16 17:17:16 +0200

    unsupervised text classification

--- text_classification.py
+++ text_classification.py
@@ -1,25 +1,46 @@
-import pandas as pd
-import sys, os
-sys.path.append(os.getcwd() + '/')
-from utilities import *
-from sklearn.manifold import TSNE
-import ast
-import numpy as np
-import matplotlib.pyplot as plt
-from scipy.spatial.distance import cdist
-import seaborn as sns
+"""
+This file performs the unsupervised text classification based on K-Means algorithm by selecting the best number of classification categories based on Silhouette metric.
+It also analyses the results on the testing set.
+
+INPUT
+data_path, i.e. the path to the folder that contains the arXiv dataset "arxiv-metadata-oai-snapshot.json".
+
+OUTPUT
+
+tuned_scibert_cls_kmean_sil_from2022.png: silhouette score on the validation set at varying of the number of categories (Fig. 3.1 in the report).
+Image (png format). Saved in data_path/img.
+
+tuned_scibert_cls_kmeans_elbow_from2022.png: WCCS score on the validation set at varying of the number of categories (for elbow method).
+Image (png format). Saved in data_path/img.
+
+tuned_scibert_cls_kmeans_pred_k=32_from.npy: K-Means predicted labels on the testing set.
+Numpy vector of dimension n_test_samples. Saved in data_path.
+
+tuned_scibert_cls_kmeans_k=32_TSNE_from2022.png: K-Means clusters on testing set (Fig. 3.2 in the report).
+Image (png format). Saved in data_path/img.
+
+tuned_scibert_cls_kmean=32_count_categories.png: bar chart reporting the number of categories associated to cluster samples (Fig. 3.3 in the report).
+Image (png format). Saved in data_path/img.
+
+tuned_scibert_cls_test_classes_evaluation.txt: 3 most frequency categories (and their frequency) for each cluster.
+Txt file. Saved in data_path.
+"""
+
+import sys
+import os
 from sklearn.model_selection import train_test_split
 from kneed import KneeLocator
+sys.path.append(os.getcwd() + '/')
+from utilities import *
 
 ################
 # DATA READING #
 # ##############
 
-data_path = '/home/rosannaturrisi/storage/NLP/'
+data_path = str(sys.argv[1])
 min_year = 2022
-#data = pd.read_csv(data_path + 'preprocessed_data_from' + str(min_year) + '.csv')
 
-name = 'tuned_scibert'
+name = 'tuned_scibert_cls'  # change it to "tuned_scibert" to perform text classification on Fine-Tuned SciBert (T)
 learn_embedding = np.load(data_path + 'finetuning/' + name + '_train_embedding_from' + str(min_year) + '.npy')
 test_embedding = np.load(data_path + 'finetuning/' + name + '_test_embedding_from' + str(min_year) + '.npy')
 y_learn = np.load(data_path + 'finetuning/' + 'y_train.npy')
@@ -44,10 +65,10 @@ kbest, ymax = k_values[np.argmax(sil_values)], np.max(sil_values)
 plt.figure(figsize=(16, 9))
 plt.plot(sil_values, "o-")
 plt.plot(np.argmax(sil_values), ymax, '*', color='red')
-plt.xlabel("k clusters", fontsize=25)
+plt.xlabel("N clusters", fontsize=25)
 plt.ylabel("silhouette", fontsize=25)
 plt.xticks(np.arange(0, len(k_values), 1), k_values)
-plt.yticks(fontsize=15)
+plt.yticks(fontsize=25)
 plt.gca().xaxis.grid(True)
 plt.savefig(data_path + 'img/' + name + '_kmean_sil_from' + str(min_year) + '.png', bbox_inches='tight')
 
@@ -68,76 +89,61 @@ plt.savefig(data_path + 'img/' + name + '_kmeans_elbow_from' + str(min_year) + '
 
 
 ##
-# Clustering with best number of k
+# Clustering with best number of clusters
 
+# Train K-Means with kbest classes
 kmeans = KMeans(n_clusters=kbest, random_state=42).fit(learn_embedding)
+
+# Evaluation on testing set
 test_pred = kmeans.predict(test_embedding)
-np.save(data_path + name + '_kmeans_pred_k='+ str(kbest) + '_from' + str(min_year) + '.npy', test_pred)
 print('Silhouette on testing:', metrics.silhouette_score(test_embedding, test_pred))
-##
+np.save(data_path + name + '_kmeans_pred_k=' + str(kbest) + '_from' + str(min_year) + '.npy', test_pred)
+
 # Cluster visualization in 2D
+cluster_visualization(test_embedding, test_pred, data_path, name, kbest)
 
-tsne = TSNE(random_state=42, init='pca')
-embedding_2D = tsne.fit_transform(test_embedding)
-
-# plot
-plt.figure(figsize=(16, 9))
-sns.set(rc={'figure.figsize': (15, 15)})
-palette = sns.hls_palette(kbest, l=.4, s=.9)  # colors
-sns.scatterplot(x=embedding_2D[:,0], y=embedding_2D[:,1], hue=test_pred, legend='full',
-                palette=palette)
-
-plt.title('TSNE with Kmeans Labels', fontsize=30)
-plt.xticks([])
-plt.yticks([])
-plt.savefig(data_path + 'img/' + name + '_kmeans_k=' + str(kbest) + '_TSNE_from' + str(min_year) + '.png',
-            bbox_inches='tight')
-
-##
-
-# Cluster evaluation
-
-plt.figure(figsize=(18, 20))
-i = 1
-for c in range(max(test_pred) + 1):
-    papers_c = np.where(test_pred == c)[0]
-    labels_c = np.array(y_test)[papers_c]
-    count_c = np.sum(labels_c, 0)
-    plt.subplot(7, 5, i)
-    plt.title('Cluster ' + str(i-1), fontsize=20)
-    plt.bar(np.arange(1, 41), list(count_c))
-    plt.ylim(0, 601)
-    if (i-1) % 5 == 0:
-        plt.yticks(np.arange(0, 601, 100), fontsize=20)
-        plt.ylabel('N. papers', fontsize=20, labelpad=10)
-    else:
-        plt.yticks(np.arange(0, 601, 100), [])
-    if i > 30:
-        plt.xticks(np.arange(0, 41, 10), fontsize=20)
-        plt.xlabel('Subject Category', fontsize=20, labelpad=10)
-    else:
-        plt.xticks(np.arange(0, 41, 10), [])
-    i += 1
-
-plt.subplots_adjust(hspace=0.4, wspace=0.2)
-plt.savefig(data_path + 'img/' + name + '_kmeans_k='+str(kbest) + '_count_categories.png', bbox_inches='tight')
 
 ##
 # Check most frequent categories within each cluster
+
+# plot bar chart for category frequency in each cluster
+plot_categories_in_clusters(test_pred, y_test, data_path, name, kbest, nrow=8, ncol=4)
+
 unique_categories_dict = np.load(data_path + 'unique_categories_dictionary_from' + str(min_year) + '.npy',
                                  allow_pickle=True).item()
 
-logfile = open(data_path + name + 'test_classes_evaluation.txt', 'w')
+macro_cat = []
+logfile = open(data_path + name + '_test_classes_evaluation.txt', 'w')
 for c in range(kbest):
     papers_c = np.where(test_pred == c)[0]
     labels_c = np.array(y_test)[papers_c]
     count_c = np.sum(labels_c, 0)
     sorted_indices = np.argsort(count_c)[::-1]
     logfile.write('\nCluster ' + str(c) + '\n')
+    macro = []
     for i in sorted_indices[:3]:
-        name = list(unique_categories_dict.keys())[i]
-        logfile.write('{} ({}):{:d}. '.format(category_map[name], name, int(count_c[i])))
+        cat_name = list(unique_categories_dict.keys())[i]
+        logfile.write('{} ({}):{:d}. '.format(category_map[cat_name], cat_name, int(count_c[i])))
+        if count_c[i] > 9:
+            macro.append(cat_name.split('.')[0].split('-')[0])
+    macro_cat.append(macro)
     logfile.write('\n')
     logfile.flush()
 
+
+one_cat, two_cat, three_cat= 0, 0, 0
+for m in macro_cat:
+    unique_macro_c = len(list(set(m)))
+    if unique_macro_c == 1:
+        one_cat += 1
+    elif unique_macro_c == 2:
+        two_cat += 1
+    else:
+        three_cat += 1
+
+logfile.write('\nOne category or completely matching categories: {}%\n'.format(one_cat/(one_cat+two_cat+three_cat)))
+logfile.write('Two main categories: {}%\n'.format(two_cat/(one_cat+two_cat+three_cat)))
+logfile.write('Three distinct categories: {}%'.format(three_cat/(one_cat+two_cat+three_cat)))
 logfile.close()
+
+
