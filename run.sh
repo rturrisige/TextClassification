979e76bf4184b6676c943e8b1b4118388041428b
rturrisi <rosanna.turrisi@edu.unige.it>
2023-05-16 17:18:05 +0200

    run example file

--- run.sh
+++ run.sh
@@ -2,4 +2,15 @@ path='/home/rosannaturrisi/storage/NLP/'
 
 echo 'Path to data:' $path
 
-python data_exploration.py $path
+echo 'Starting data analysis and processing'
+python data_analysis_and_processing.py $path
+
+echo 'Extracting PCA-SciBert embedding'
+python PCA_SciBert_embedding.py $path
+
+echo 'Fine-tuning of SciBert model and embedding extraction'
+python FineTuned_SciBert_embedding.py $path
+
+echo 'Unsupervised text classification'
+python text_classification.py $path
+
